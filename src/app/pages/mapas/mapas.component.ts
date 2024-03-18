d93620318335d9f1c14343edd6e13eaccf5ecdf0
matildejb <matildejimenezbedmar@gmail.com>
2024-03-18 20:23:57 +0100

    Configuracion de mapa con posicion y centrado

--- src/app/pages/mapas/mapas.component.ts
+++ src/app/pages/mapas/mapas.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-mapas',
   standalone: true,
-  imports: [],
+  imports: [GoogleMap, MapMarker],
   templateUrl: './mapas.component.html',
   styleUrl: './mapas.component.css'
 })
 export class MapasComponent {
+  myPosition = signal<any>(""); 
+
+  ngOnInit(){
+    navigator.geolocation.getCurrentPosition( position => {
+      let center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+      this.myPosition.set(center)
+    })
+  }
 
 }
