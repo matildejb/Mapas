e7a695140901441d86c85ffa29392dd110f5078f
matildejb <matildejimenezbedmar@gmail.com>
2024-03-18 19:52:56 +0100

    Componentes, servicios, interface y rutas
    Elementos iniciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MapasComponent } from './pages/mapas/mapas.component';
+import { CountryViewComponent } from './pages/country-view/country-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo: "mapa" },
+    { path: "mapa", component: MapasComponent},
+    { path: "country/:code", component: CountryViewComponent},
+];
