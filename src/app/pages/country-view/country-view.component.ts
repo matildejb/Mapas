24e1d197f4931a8337e818524872cd38abe0d87f
matildejb <matildejimenezbedmar@gmail.com>
2024-03-19 16:52:55 +0100

    Terminamos maquetación mapa

--- src/app/pages/country-view/country-view.component.ts
+++ src/app/pages/country-view/country-view.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { ICountry } from '../../interfaces/icountry.interface';
 
 @Component({
   selector: 'app-country-view',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe, RouterLink],
   templateUrl: './country-view.component.html',
   styleUrl: './country-view.component.css'
 })
 export class CountryViewComponent {
+  activatedRoute = inject(ActivatedRoute)
+  countriesServices = inject(CountriesService)
+  country!: ICountry
+
+  ngOnInit(){
+    this.activatedRoute.params.subscribe(async (params: any) => {
+      let cca2 = params.code
+      let response = await this.countriesServices.getByCode(cca2)
+      console.log(response) // me llega un array de una sola posicion tengo que   coger la posicion 0
+      this.country = response[0]
+    })
+  }
 
 }
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { ICountry } from '../interfaces/icountry.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+  private httpClient = inject(HttpClient)
+  private baseUrl: string = "https://restcountries.com/v3.1/"
+
+ getAll( region = "europe" ): Promise<ICountry[]>{
+  return lastValueFrom(this.httpClient.get<ICountry[]>(`${this.baseUrl}region/${region}`))
+  
+ }
+
+ getByCode(code: string): Promise<ICountry[]>{
+  return lastValueFrom(this.httpClient.get<ICountry[]>(`${this.baseUrl}alpha/${code}`))
+ }
 }
